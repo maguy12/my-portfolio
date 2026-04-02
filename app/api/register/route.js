c5f0a9c0225da53af8516246e4552276402c793a
maguy12 <kamangomaguy5@gmail.com>
2026-04-02 04:01:15 +0100

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,28 +1,78 @@
-import { getDB } from "../../../lib/db";
+"use client";
 
-export async function POST(req){
+import { useState } from "react";
 
-  const { name, email, password } = await req.json();
+export default function Register(){
 
-  if(!name || !email || !password){
-    return Response.json({message:"Champs manquants ❌"});
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  async function register(){
+
+    console.log("CLICK REGISTER");
+
+    if(!name || !email || !password){
+      alert("Remplis tous les champs ❌");
+      return;
+    }
+
+    try{
+      const res = await fetch("/api/register",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({name,email,password})
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
+      alert(data.message);
+
+      if(data.message.includes("créé")){
+        window.location.href="/login";
+      }
+
+    }catch(err){
+      console.error(err);
+      alert("Erreur serveur ❌");
+    }
   }
 
-  const db = await getDB();
+  return(
+    <div className="container">
 
-  const exist = await db.get(
-    "SELECT * FROM users WHERE email = ?",
-    [email]
+      <h1>📝 Inscription</h1>
+
+      <input
+        placeholder="Nom"
+        onChange={(e)=>setName(e.target.value)}
+      />
+
+      <input
+        placeholder="Email ou numéro"
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        onChange={(e)=>setPassword(e.target.value)}
+      />
+
+      <button onClick={register}>
+        S'inscrire
+      </button>
+
+      <p>Déjà un compte ?</p>
+
+      <a href="/login">
+        <button>Se connecter</button>
+      </a>
+
+    </div>
   );
-
-  if(exist){
-    return Response.json({message:"Email déjà utilisé ❌"});
-  }
-
-  await db.run(
-    "INSERT INTO users(name,email,password) VALUES(?,?,?)",
-    [name,email,password]
-  );
-
-  return Response.json({message:"Compte créé 🔥"});
-}
+        }
